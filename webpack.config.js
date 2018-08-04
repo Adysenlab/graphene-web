a13be58134d8b47e72c7c118526f9d1246ecccb1
Aditya Aloy Sen <aloysen951@gmail.com>
2018-08-04 12:55:12 +0530

    adding jquery and change index

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
@@ -65,6 +66,10 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
         extractPlugin,
         new HtmlWebpackPlugin({
             template: 'src/index.html'
